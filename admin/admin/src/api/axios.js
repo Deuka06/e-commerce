bce064c3bff7eed1c247f36eeb7433bd656e1cf7
ALish <sharipkhanalikhan@gmail.com>
2026-03-06 16:30:11 +0500

    Admin have changes with axios and path of server

--- admin/admin/src/api/axios.js
+++ admin/admin/src/api/axios.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://46.247.41.196/api/v1", // Сіздің сервердің адресі
+const api = axios.create({
+  // Swagger-дегі Production Server мекенжайы
+  baseURL: "/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // Сұраныс жіберлер алдында токенді автоматты түрде қосу
-instance.interceptors.request.use((config) => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
